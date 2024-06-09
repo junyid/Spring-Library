a9b2eac664651bd0d3bc3aa6c83d2f0a16029216
Junyi Dong <60515732+junyid@users.noreply.github.com>
2024-06-08 19:10:35 -0700

    Spring project init

---
+++ Back-end/the-spring-library/src/main/java/com/junyid/the_spring_library/TheSpringLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.junyid.the_spring_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSpringLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSpringLibraryApplication.class, args);
+	}
+
+}
---
+++ Back-end/the-spring-library/src/test/java/com/junyid/the_spring_library/TheSpringLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junyid.the_spring_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSpringLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
